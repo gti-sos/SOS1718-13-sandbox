ab38f3be86477899e775c0aa856f625ac1349b58
Alfonso Gutierrez Romero <alfonso.g.r21@gmail.com>
2018-04-01 23:42:39 +0000

    mongodb funcionando AGR

--- fonedriversApi/index.js
+++ fonedriversApi/index.js
@@ -8,26 +8,26 @@ fonedriversApi.register = function(app, db, initialF_one_drivers) {
 
 
     app.get(BASE_API_PATH+"/f-one-drivers/loadInitialData",(req,res)=>{
-    console.log(Date() + " - Trying to load 2 drivers");
+    console.log(Date() + " - Trying to load 5 drivers");
     
     //find{name: "loquesea"} o {} para todos
-db.find({},(err,f_one_drivers)=>{
+db.find({}).toArray((err,f_one_drivers)=>{
     if(err){
         console.error("Error acceso DB");
         res.sendStatus(500);
                 return;
         //process.exit(1);//Cierra el servidor
     }
-    
     if(f_one_drivers.length==0){
         console.log("Empty DB");
         db.insert(initialF_one_drivers);
         console.log("DB initialized with " + f_one_drivers.length + " drivers" );
         res.sendStatus(201);
+        
     }else{
         console.log("DB already have " + f_one_drivers.length + " drivers" );
     }
-    
+    res.redirect(BASE_API_PATH+"/f-one-drivers");
 });
 
 });
@@ -35,14 +35,14 @@ db.find({},(err,f_one_drivers)=>{
 app.get(BASE_API_PATH+"/f-one-drivers",(req,res)=>{
     console.log(Date() + " - GET / f-one-drivers");
     
-    db.find({},(err,f_one_drivers)=>{
+    db.find({}).toArray((err,f_one_drivers)=>{
     if(err){
         console.error("Error acceso DB");
         res.sendStatus(500);
         return;
     }
         
-    res.send(f_one_drivers);
+    res.send(f_one_drivers.map((c)=>{delete c._id;return c;}));
     });
 
 });
@@ -52,13 +52,13 @@ app.get(BASE_API_PATH+"/f-one-drivers/:year",(req,res)=>{
     var year = req.params.year;
     
     console.log(Date() + " - GET / f-one-drivers/" + year);
-    db.find({year : parseInt(year)},(err,driver)=>{
+    db.find({"year" : parseInt(year)}).toArray((err,driver)=>{
     if(err){
         console.error("Error acceso DB");
         res.sendStatus(500);
         return;
     }
-    res.send(driver);
+    res.send(driver.map((c)=>{delete c._id;return c;}));
     });
     
     /*res.send(f_one_drivers.filter((c)=>{
@@ -111,7 +111,7 @@ app.put(BASE_API_PATH+"/f-one-drivers/:year",(req,res)=>{
         console.warn(Date() + " - Hacking attempt!");
         return;
     }
-    db.update({year : parseInt(driver.year,10)},driver,(err,numUpdate)=>{console.log("Updated: " + numUpdate);});
+    db.update({"year" : parseInt(driver.year,10)},driver,(err,numUpdate)=>{console.log("Updated: " + numUpdate);});
 
     /*f_one_drivers = f_one_drivers.map((c)=>{
         if(c.year==driver.year)
